d406e17e9f1304a38ea049edf1d259f8382edb87
tajansari612 <tajansari612@gmail.com>
2024-09-06 18:54:46 +0530

    new topic added

---
+++ 01_basics/01_hello/hello.js
@@ -0,0 +1,15 @@
+console.log("Hello, this is nodejs tutorial")
+
+// console.log(window)  //error removed in javascript server (node)
+
+// alert("Hello") //error removed in javascript server (node)
+
+// document.getElementById("") // all commands like this will not work because it
+                              //  has been removed from javascript
+// DOM, Window object, ui handling commands, query commands, etc all are removed
+
+// run "npm init" in terminal  //it manages the external packages and use
+// to add user scripts by creating package.json file
+
+//For ex. adding dev dependency in scripts to run js file using npm
+// npm run dev   //in terminal executes same as like node hello.js
