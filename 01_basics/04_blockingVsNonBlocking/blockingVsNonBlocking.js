509cb14bec18333fa66a0c8c6af7dc2f753508d5
tajansari612 <tajansari612@gmail.com>
2024-09-20 00:26:43 +0530

    new topic added

---
+++ 01_basics/04_blockingVsNonBlocking/blockingVsNonBlocking.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+//blocking 
+console.log('1');
+console.log(fs.readFileSync('contacts.txt', 'utf-8'))  //code below is executed even if this is not executed
+console.log('2');
+console.log('3');
+console.log('4');
+
+//non-blocking
+console.log('1');
+console.log(fs.readFile('contacts.txt', 'utf-8', (err) => {}))  //code below is not executed untill it compeletes
+console.log('2');
+console.log('3');
+console.log('4');
