55a59da8eaa12a17ef16cc7782626bf36f28384d
tajansari612 <tajansari612@gmail.com>
2024-10-17 00:18:02 +0530

    fixed minor issue

--- 01_basics/04_blockingVsNonBlocking/blockingVsNonBlocking.js
+++ 01_basics/04_blockingVsNonBlocking/blockingVsNonBlocking.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 
 //blocking 
 console.log('1');
-console.log(fs.readFileSync('contacts.txt', 'utf-8'))  //code below is executed even if this is not executed
+console.log(fs.readFileSync('contacts.txt', 'utf-8'))  //code below is not executed untill it compeletes
 console.log('2');
 console.log('3');
 console.log('4');
 
 //non-blocking
 console.log('1');
-console.log(fs.readFile('contacts.txt', 'utf-8', (err) => {}))  //code below is not executed untill it compeletes
+console.log(fs.readFile('contacts.txt', 'utf-8', (err) => {}))  //code below is executed even if this is not executed
 console.log('2');
 console.log('3');
 console.log('4');
